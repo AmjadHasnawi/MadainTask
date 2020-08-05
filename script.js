0d7f58e298ac07e86e087a0c52783c4826adb74d
AmjadHasnawi <civilamjad227@gmail.com>
2020-08-05 20:35:44 +0300

    improve and clean the code

--- script.js
+++ script.js
@@ -1,96 +1,67 @@
-    var arr = [{value: ''},{value: ''},{value: ''},{value: ''},{value: ''},{value: ''},{value: ''},{value: ''},{value: ''}]
-    
-    function myTurn(ele, num) {
-        if (ele && !ele.innerHTML) {
-            ele.innerHTML = "X";
-            arr[num].value = "X";
-            
-        }
-        checkForWhen();
+let arr = ['', '', '', '', '', '', '', '', '']
+let X = 'X';
+let O = 'O';
+
+function myTurn(ele, num) {
+    if (!ele.innerHTML) {
+        ele.innerHTML = X;
+        arr[num] = X;  
+    } else {
+        alert('Please choose another spot');
+        return;
+    }
+    setTimeout(() => {
+        if (checkForWin()) {
+            return;
+        };
         computersTurn();
+    }, 100);  
+}
+
+function computersTurn() {
+    for (let i = 0; i < 9; i++) {
+        let num = Math.floor(Math.random() * 9)
+        let choice = 'td' + num;
+        let chosen =  document.getElementById(choice);
+        if(!chosen.innerHTML) {
+            document.getElementById(choice).innerHTML = O;
+            arr[num] = O;
+            setTimeout(() => {
+                checkForWin();
+            }, 100);
+            break;
+        }
+    }   
+};
+
+function checkForWin() {
+    if ((arr[0] === arr[1]) && (arr[1] === arr[2])) {
+       return whoWin(0);
+    } else if ((arr[0] === arr[3]) && (arr[3] === arr[6])) {
+        return whoWin(0);
+    } else if ((arr[0] === arr[4]) && (arr[4] === arr[8])) {
+        return whoWin(0);
+    } else if ((arr[3] === arr[4]) && (arr[4] === arr[5])) {
+        return whoWin(3);
+    } else if ((arr[6] === arr[7]) && (arr[7] === arr[8])) {
+        return whoWin(6);
+    } else if ((arr[1] === arr[4]) && (arr[4] === arr[7])) {
+        return whoWin(1);
+    } else if ((arr[2] === arr[5]) && (arr[5] === arr[8])) {
+        return whoWin(2);
+    } else if ((arr[2] === arr[4]) && (arr[4] === arr[6])) {
+        return whoWin(2);
+    }    
+}
+
+function whoWin(num) {
+    if (arr[num] === X) {
+        alert('You Win');
+        window.location.reload();
+        return true;
+    } else if (arr[num] === O) {
+        alert('Computer Win');
+        window.location.reload();
+        return true;
     }
-
-    function computersTurn() {
-        for (var i = 0; i < 9; i++) {
-            var num = Math.floor(Math.random() * 10)
-            var choice = 'div' + num;
-             var chosen =  document.getElementById(choice);
-            if(chosen && !chosen.innerHTML) {
-                document.getElementById(choice).innerHTML = "Y";
-                arr[num].value = "Y";
-                checkForWhen();
-                break;
-            }
-        }
-        for (var i = 0; i < 9; i++) {
-            console.log( i + ''+ arr[i].value);}
-    };
- function checkForWhen() {
-     console.log(arr[0].value , arr[1].value , arr[2].value);
-     if ((arr[0].value === arr[1].value) && (arr[1].value === arr[2].value)) {
-        if (arr[0].value === 'X') {
-            alert('You Win');
-        } else if (arr[0].value === 'Y') {
-            alert('Computer Win');
-        }
-     }
-
-    if ((arr[0].value === arr[3].value) && (arr[3].value === arr[6].value)) {
-        if (arr[0].value === 'X') {
-            alert('You Win');
-        }  else if (arr[0].value === 'Y') {
-            alert('Computer Win');
-        }
-    }
-
-    if ((arr[0].value === arr[4].value) && (arr[4].value === arr[8].value)) {
-        if (arr[0].value === 'X') {
-            alert('You Win');
-        }  else if (arr[0].value === 'Y') {
-            alert('Computer Win');
-        }
-    }
-
-    if ((arr[3].value === arr[4].value) && (arr[4].value === arr[5].value)) {
-        if (arr[3].value === 'X') {
-            alert('You Win');
-        }  else if (arr[3].value === 'Y') {
-            alert('Computer Win');
-        }
-    }
-
-    if ((arr[6].value === arr[7].value) && (arr[7].value === arr[8].value)) {
-        if (arr[6].value === 'X') {
-            alert('You Win');
-        }  else if (arr[6].value === 'Y') {
-            alert('Computer Win');
-        }
-    }
-
-    if ((arr[1].value === arr[4].value) && (arr[4].value === arr[7].value)) {
-        if (arr[1].value === 'X') {
-            alert('You Win');
-        }  else if (arr[1].value === 'Y') {
-            alert('Computer Win');
-        }
-    }
-
-    if ((arr[2].value === arr[5].value) && (arr[5].value === arr[8].value)) {
-        if (arr[2].value === 'X') {
-            alert('You Win');
-        }  else if (arr[2].value === 'Y') {
-            alert('Computer Win');
-        }
-    }
-
-    if ((arr[2].value === arr[4].value) && (arr[4].value === arr[6].value)) {
-        if (arr[2].value === 'X') {
-            alert('You Win');
-        }  else if (arr[2].value === 'Y') {
-            alert('Computer Win');
-        }
-    }
-    
-
-     
- }
+}
